26aa668743226a1a25c6e3152be7401e8720fc53
jenoLA <jeno.andrade@gmail.com>
2019-07-28 13:37:31 -0300

    some nices changes

--- config.h
+++ config.h
@@ -1,22 +1,22 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 15;       /* gaps between windows  */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "-*-notosans nerd fonts-*-*-*-*-*-*-*-*-*-*-*-*-*:size=12" };
 //static const char dmenufont[]       = "monospace:size=10"; // using rofia script, no need to have this
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char norm_bg[]         = "#011";//222222
+static const char norm_border[]     = "#aaa";
+static const char norm_fg[]         = "#bbb";
+static const char sel_fg[]          = "#528";
+static const char sel_border[]      = "#528";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeNorm] = { norm_fg, norm_bg, norm_border  },
+	[SchemeSel]  = { sel_fg, norm_bg, sel_border },
 };
 
 /* tagging */
@@ -35,7 +35,8 @@ static const Rule rules[] = {
 	{ "Telegram",     NULL,   NULL,       1 << 2,       0,           -1 },
 	{ "URxvt",        NULL,   NULL,       0,            1,           -1 },
 	{ "Anki",         NULL,   NULL,       1 << 5,       0,           -1 },
-	{ "mpv",          NULL,   NULL,       1 << 4,       0,           -1 },
+	{ "Steam",        NULL,   NULL,       1 << 4,       0,           -1 },
+	{ "mpv",          NULL,   NULL,       1 << 4,       1,           -1 },
 };
 
 /* layout(s) */
@@ -64,20 +65,21 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-//static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-//static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *roficmd[]  = { "rofia",   NULL }; // using a script to spawn rofi with desired args
-static const char *termcmd[]  = { "st",      NULL };
+static const char *termcmd[]  = { "kitty",   NULL };
 static const char *anki[]     = { "anki",    NULL };
 static const char *URdraw[]   = { "urdraw",  NULL }; // script to draw urxvt
 static const char *slock[]    = { "slock",   NULL };
-static const char *qutebrowser[]  = { "qutebrowser",  NULL };
+static const char *steam[]    = { "steam",   NULL };
+static const char *browser[]  = { "qutebrowser",  NULL };
 static const char *telegram[] = { "telegram-desktop", NULL };
 
+// Mod1Mask == alt key    &&  Mod4Mask == super key
 static Key keys[] = {
 	/* modifier             key    function        argument              use "xev | grep key" to find out the keys */
-	{ KeyPress, MODKEY,               40,    spawn,          {.v = roficmd    } }, // d
-	{ KeyPress, Mod1Mask,             56,    spawn,          {.v = qutebrowser} }, // b
+	{ KeyPress, Mod1Mask,             40,    spawn,          {.v = roficmd    } }, // d
+	{ KeyPress, Mod1Mask,             39,    spawn,          {.v = steam      } }, // s
+	{ KeyPress, Mod1Mask,             56,    spawn,          {.v = browser    } }, // b
 	{ KeyPress, Mod1Mask,             28,    spawn,          {.v = telegram   } }, // t
 	{ KeyPress, MODKEY,               36,    spawn,          {.v = termcmd    } }, // return
 	{ KeyPress, Mod1Mask,             36,    spawn,          {.v = URdraw     } }, // return
@@ -118,24 +120,23 @@ static Key keys[] = {
 	TAGKEYS(                16,                    6)                    // 7
 	TAGKEYS(                17,                    7)                    // 8
 	TAGKEYS(                18,                    8)                    // 9
-	{ KeyPress, MODKEY,               24,    quit,           {0} },                // q
-	{ KeyPress, MODKEY,               27,    quit,           {.i = 1} },           // gonna [R]estart dwm
+	{ KeyPress, MODKEY,     24,    quit,           {0} },                // q
+	{ KeyPress, MODKEY,     27,    quit,           {.i = 1} },           //[R]estart dwm
    					
 					/* scripts and others modifications */
     	// audioCtrl set volume up/down 5% and mute/unmute 
-	{ KeyPress, Mod1Mask,             123,   spawn,         SHCMD("~/bin/audioCtrl 0")   }, // raise volume buttom
-	{ KeyPress, Mod1Mask,             122,   spawn,         SHCMD("~/bin/audioCtrl 1")   }, // lower volume buttom
-	{ KeyPress, Mod1Mask,             121,   spawn,         SHCMD("~/bin/audioCtrl 2")   }, // audio mute/unmute
+	{ KeyPress, 0,             123,   spawn,         SHCMD("~/bin/audioCtrl 0")   }, // raise volume buttom
+	{ KeyPress, 0,             122,   spawn,         SHCMD("~/bin/audioCtrl 1")   }, // lower volume buttom
+	{ KeyPress, 0,             121,   spawn,         SHCMD("~/bin/audioCtrl 2")   }, // audio mute/unmute
 	//  musicCtrl controls mpd via mpc
-	{ KeyPress, Mod1Mask,             172,   spawn,         SHCMD("~/bin/musicCtrl 0")   }, // Play buttom
-	{ KeyPress, Mod1Mask,             173,   spawn,         SHCMD("~/bin/musicCtrl 2")   }, // previous buttom
-	{ KeyPress, Mod1Mask,             171,   spawn,         SHCMD("~/bin/musicCtrl 1")   }, // next buttom 
-	{ KeyPress, Mod1Mask,             58,    spawn,         SHCMD("~/bin/musicCtrl 3")   }, // m  notify music playing
+	{ KeyPress, 0,             172,   spawn,         SHCMD("~/bin/musicCtrl 0")   }, // Play buttom
+	{ KeyPress, 0,             173,   spawn,         SHCMD("~/bin/musicCtrl 2")   }, // previous buttom
+	{ KeyPress, 0,             171,   spawn,         SHCMD("~/bin/musicCtrl 1")   }, // next buttom 
+	{ KeyPress, Mod1Mask,      58,    spawn,         SHCMD("~/bin/musicCtrl 3")   }, // m  notify music playing
 	// another nice things
-	{ KeyPress, Mod1Mask,             107,   spawn,         SHCMD("~/bin/screeny")  },             // print buttom
-	{ KeyPress, Mod1Mask,             27,    spawn,         SHCMD("~/bin/record" )  },             // r
-	{ KeyPress, Mod1Mask,             39,    spawn,         SHCMD("~/bin/stoprec")  },             // s
-	{ KeyPress, Mod1Mask,             29,    spawn,         SHCMD("~/bin/ytmp3 '($xclip -out)'")}, // y
+	{ KeyPress, 0,             107,   spawn,         SHCMD("~/bin/screeny")  },             // print buttom
+	{ KeyPress, Mod1Mask,      27,    spawn,         SHCMD("~/bin/record" )  },             // r
+	{ KeyPress, Mod1Mask,      29,    spawn,         SHCMD("~/bin/ytmp3 '($xclip -out)'")}, // y
 };
 
 /* button definitions */
